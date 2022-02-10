eae522fee16580b229194e6098534f7026d6abc1
Mastan25 <98580486+Mastan25@users.noreply.github.com>
2022-02-10 13:55:28 +0530

    Add files via upload

--- Mainlogic.java
+++ Mainlogic.java
@@ -1,27 +1,40 @@
- package com.mondee;
+package com.mondee;
 
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
-import org.hibernate.cfg.Configuration; 
+import org.hibernate.boot.Metadata;
+import org.hibernate.boot.MetadataBuilder;
+import org.hibernate.boot.registry.StandardServiceRegistry;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.metadata.ClassMetadata;
+
 
 public class Mainlogic {
 
 	
 	public static void main(String[] args) {
-
-		Configuration cf = new Configuration();
-		cf.configure("hibernet.xml");
-		@SuppressWarnings("deprecation")
-		SessionFactory sf = cf.buildSessionFactory();
+		// TODO Auto-generated method stub
+       StandardServiceRegistry ssr=new StandardServiceRegistryBuilder().configure("hibernet.xml").build();
+       Metadata me=new org.hibernate.boot.MetadataSources(ssr).getMetadataBuilder().build();
+		SessionFactory sf = me.buildSessionFactory();
 		Session se = sf.openSession();
 		Transaction tx = se.beginTransaction();
-		student mastan = new student();
-		mastan.setSid(9999);
-		mastan.setSname(" dudekula vali");
-	    mastan.setSmarks(1000);
+		student mastan= new student();
+		mastan.setSid(1206);
+		mastan.setSname("surya");
+		mastan.setSmarks(700);
+		
 		se.save(mastan);
+		
+		
 		tx.commit();
 		se.close();
-		}	
+		
+		
+		
+		
 	}
+
+}
--- student.java
+++ student.java
@@ -1,3 +1,4 @@
+
 package com.mondee;
 
 public class student 
